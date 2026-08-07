f450d7c229536ef859251b816bdb9e8ede802eab
Muadz <muadz.uk@gmail.com>
2026-08-07 19:09:15 +0100

    Made a start on day 4

---
+++ day04/strings.py
@@ -0,0 +1,16 @@
+# Concatenation of strings
+concat_str = "Thirty " + "Days " + "Of " + "Python"
+concat_coding = "Coding " + "For " + "All"
+
+company = "Coding For All"
+print(company)
+print(len(company))
+print(company.upper())
+print(company.lower())
+
+# Different string methods
+print(company.capitalize())
+print(company.title())
+print(company.swapcase())
+
+print(company[7:12])
