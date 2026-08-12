0a4a54dc68b172b37b96f96ebac135664b7a023a
Muadz <muadz.uk@gmail.com>
2026-08-12 16:25:07 +0100

    Level 3 Day 12 exercises completed

--- day12/modules.py
+++ day12/modules.py
@@ -24,3 +24,49 @@ def rgb_color_gen():
 
 print(rgb_color_gen())
 
+'''
+Level 2 Exercises
+'''
+
+hex_char = string.hexdigits
+
+def list_of_hexa_colors(num_of_colors):
+    return [ '#' + ''.join( hex_char[randint(0, len(hex_char) - 1)] for _ in range(6)) for _ in range(num_of_colors)]
+
+print(list_of_hexa_colors(5))
+
+def list_of_rgb_colors(num_of_colors):
+    return [rgb_color_gen() for _ in range(num_of_colors)]
+
+print(list_of_rgb_colors(5))
+
+def generate_colours(type, num_of_colors):
+    if type == 'hex':
+        return list_of_hexa_colors(num_of_colors)
+    elif type == 'rgb':
+        return list_of_rgb_colors(num_of_colors)
+    else:
+        return None
+
+print(generate_colours('hex', 5))
+print(generate_colours('rgb', 5))
+
+'''
+Level 3 Exercises
+'''
+
+from random import shuffle
+
+def shuffle_list(mylist):
+    shuffle(mylist)
+    return mylist
+
+print(shuffle_list([1, 2, 3, 4, 5]))
+
+def random_array():
+    new_set = set()
+    while len(new_set) < 7:
+        new_set.add(randint(0, 9))
+    return list(new_set)
+
+print(random_array())
