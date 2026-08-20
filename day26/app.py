0c18e590a50362343f2bbe11ab9f6a17afff501b
Muadz <muadz.uk@gmail.com>
2026-08-20 11:06:01 +0100

    Day 27 examples done

---
+++ day26/app.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, request, redirect, url_for
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    techs = ['HTML', 'CSS', 'Python', 'Flask']
+    name = '30 Days of Python Programming'
+    return render_template('home.html', name=name, techs=techs)
+@app.route('/about')
+def about():
+    name = '30 Days of Python Programming'
+    return render_template('about.html')
+
+@app.route('/post', methods=['GET','POST'])
+def post():
+    name = 'Text Analyzer'
+    if request.method == 'GET':
+        return render_template('post.html', name = name, title = name)
+    if request.method =='POST':
+        content = request.form['content']
+        print(content)
+        return redirect(url_for('result'))
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
+
