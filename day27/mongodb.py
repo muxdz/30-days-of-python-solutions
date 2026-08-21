8c1596e488d703342fcbe9f4d05f0d28691db889
Muadz <muadz.uk@gmail.com>
2026-08-21 11:47:16 +0100

    Day 27 exercises done

--- day27/mongodb.py
+++ day27/mongodb.py
@@ -3,6 +3,7 @@ import os
 
 from pathlib import Path
 from dotenv import load_dotenv
+from bson.objectid import ObjectId
 
 env_path = Path(__file__).resolve().parent / '.env'
 load_dotenv(dotenv_path=env_path)
@@ -10,13 +11,53 @@ load_dotenv(dotenv_path=env_path)
 MANGODB_URI = os.environ['MONGODB_URI']
 client = pymongo.MongoClient(MANGODB_URI)
 
+db = client['thirty_days_of_python'] # accessing the database
+db.students.find().limit(1)
 
-db = client.thirty_days_of_python
+db.students.drop()
 
-students = [
-        {'name':'David','country':'UK','city':'London','age':34},
-        {'name':'John','country':'Sweden','city':'Stockholm','age':28},
-        {'name':'Sami','country':'Finland','city':'Helsinki','age':25},
-    ]
+query = {
+    "country":"Finland"
+}
+students = db.students.find(query)
 for student in students:
-    db.students.insert_one(student)
+    print(students)
+
+query = {"age":{"$gt":30}}
+students = db.students.find(query)
+for student in students:
+    print(student)
+
+students = db.students.find().sort('name')
+for student in students:
+    print(student)
+
+
+students = db.students.find().sort('name',-1)
+for student in students:
+    print(student)
+
+students = db.students.find().sort('age')
+for student in students:
+    print(student)
+
+students = db.students.find().sort('age',-1)
+for student in students:
+    print(student)
+
+query = {'age':250}
+new_value = {'$set':{'age':38}}
+
+db.students.update_one(query, new_value)
+# lets check the result if the age is modified
+for student in db.students.find():
+    print(student)
+
+query = {'name':'John'}
+db.students.delete_one(query)
+
+for student in db.students.find():
+    print(student)
+# lets check the result if the age is modified
+for student in db.students.find():
+    print(student)
